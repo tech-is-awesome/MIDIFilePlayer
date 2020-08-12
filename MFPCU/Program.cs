e3830d51ebe14c452846f665922d846fd1d4f69c
Scott's Tech Logs <scott@scotttech.ml>
2020-08-12 10:40:23 +0100

    Created MFPCU (MIDI File Player Conversion Utility

---
+++ MFPCU/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MFPCUCore;
+
+namespace MIDI_File_Player_Converter_Utitlity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MIDI File Player Converter Utitlity\nBy Scott's Tech Logs 2020");
+            Console.WriteLine("Getting File");
+            double[] notes = MFPCUCore.API.notes(args[0]);
+        }
+    }
+}
